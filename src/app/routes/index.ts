84b2e47203e17cb903b920e452c1c4fa29d5e801
nafis200 <nafisahamed14@gmail.com>
2025-04-25 17:56:16 +0600

    backend done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,6 +8,8 @@ import { pdfRoute } from '../modules/pdfgenerator/pdf.route';
 import { PatientRoutes } from '../modules/Patient/patient.route';
 import { ScheduleRoutes } from '../modules/Schedule/schedule.routes';
 import { DoctorScheduleRoutes } from '../modules/DoctorSchedule/doctorSchedule.routes';
+import { AppointmentRoutes } from '../modules/Appointment/appointment.routes';
+import { PaymentRoutes } from '../modules/Payment/payment.routes';
 
 const router = express.Router();
 
@@ -47,6 +49,14 @@ const moduleRoutes = [
     {
         path: '/doctor-schedule',
         route: DoctorScheduleRoutes
+    },
+    {
+        path: '/appointment',
+        route: AppointmentRoutes
+    },
+    {
+        path: '/payment',
+        route: PaymentRoutes
     }
 ]
 
